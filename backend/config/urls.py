7269e908ab810ebb0ebd071634a39b4c7630ad77
Tejal Mhatre <tejalmhatrejk@gmail.com>
2026-08-26 21:33:09 +0530

    minor change in urls.py

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -22,3 +23,4 @@ urlpatterns += static(
     settings.MEDIA_URL,
     document_root=settings.MEDIA_ROOT
 )
+
