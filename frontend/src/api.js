9ff4e963bc3d18adc26d22808b6f1e6430edced2
Tejal Mhatre <tejalmhatrejk@gmail.com>
2026-09-11 18:14:23 +0530

    Connect frontend to deployed backend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: 'http://127.0.0.1:8000/api/',
+    baseURL: "https://campusly-backend-v7xh.onrender.com/api/",
 });
 
 // Add access token to every request
